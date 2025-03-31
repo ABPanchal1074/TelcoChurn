6d543f71b1f9fba1c1b721f94b00a666d60b01be
Aniket Panchal <154958430+ABPanchal1074@users.noreply.github.com>
2025-03-31 23:48:01 +0530

    Update real_time_monitor.py

--- real_time_monitor.py
+++ real_time_monitor.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def real_time_monitor():
     st.title("⏱️ Real-Time Churn Monitoring")
-    data = pd.read_csv(r'C:\Users\Aniket\TelcoChurn\preprocessed_data.csv')
+    data = pd.read_csv(r'preprocessed_data.csv')
     st.dataframe(data.tail(10))
 
